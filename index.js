9a07ae6d66e1c89f31aa2e5e81bbabe5dbda3fe6
Sathya Vallinayagam <sathyavalli235@gmail.com>
2025-08-02 11:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/FlipStack", {
+mongoose.connect(process.env.MONGODB_LINK, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('✅ Connected to MongoDB...'))
 .catch(err => console.error('❌ Could not connect to MongoDB...', err));
 
-const User = require('./models/User');
+const User = require('./models/user');
 
 // ✅ Signup Route (includes phone, address)
 app.post('/signup', async (req, res) => {
